6e7ec69aa02766333acd824fa4464e48d8addf30
Pranavbc <pranav1952000@gmail.com>
2025-03-16 16:43:51 +0530

    thymeleaf fragment constant ( first creating tymeleaf const code and insert and replace in another page) code using in another page

--- scm2.0-main/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0-main/src/main/java/com/scm/controllers/PageController.java
@@ -1,12 +1,19 @@
 package com.scm.controllers;
 
 
+import com.scm.Application;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+
+    private final Application application;
+
+    PageController(Application application) {
+        this.application = application;
+    }
 
     @RequestMapping("/home")
     public String home(Model model) {
@@ -19,5 +26,17 @@ public class PageController {
 
      return "home";
     }
-   
+
+
+    @RequestMapping("/about")
+    public String about(){
+     System.out.println("about page is loading");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services(){
+        System.out.println("This is services page");
+        return "services";
+    }
 }
