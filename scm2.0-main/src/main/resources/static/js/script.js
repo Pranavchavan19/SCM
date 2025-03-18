3eef15c419fd0d992dbc7cf540a8ea74a1a33896
Pranavbc <pranav1952000@gmail.com>
2025-03-18 11:48:45 +0530

    applying dark mode through button with java script

--- scm2.0-main/src/main/resources/static/js/script.js
+++ scm2.0-main/src/main/resources/static/js/script.js
@@ -1,14 +1,38 @@
 console.log("scrippt loadee");
 
 
-let currentTheme = "light";
+let currentTheme = getTheme();
+
+console.log(currentTheme);
+
+changeTheme();
 
 
 function  changeTheme(){
+ 
+    document.querySelector('html').classList.add(currentTheme);
+
+   const changeThemeButton = document.querySelector("#theme_change_button");
+
+   changeThemeButton.addEventListener("click" , (event) => {
+ 
+    console.log("button  cicked");
+    
+   })
 
 }
 
 
 function setTheme(theme){
     localStorage.setItem("theme" , theme);
+}
+
+function getTheme(){
+
+    let theme = localStorage.getItem("theme");
+   
+    // if(theme) return
+    // else  return "light";
+
+    return  theme ? theme : "light";
 }
