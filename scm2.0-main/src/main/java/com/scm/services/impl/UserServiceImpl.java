75b4dd19de2e6311cd50fed48396c759bb981eca
Pranavbc <pranav1952000@gmail.com>
2025-03-19 14:28:52 +0530

    failure comes

--- scm2.0-main/src/main/java/com/scm/services/impl/UserServiceImpl.java
+++ scm2.0-main/src/main/java/com/scm/services/impl/UserServiceImpl.java
@@ -1,5 +1,63 @@
 package com.scm.services.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.scm.entities.user;
+import com.scm.repositories.UserRepo;
+import com.scm.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    @Aut
+    private UserRepo userRepo;
+
+    @Override
+    public void deleteUser(String id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<user> getAllUsers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<user> getUserById(String id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isUserExist(String userId) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isUserExistByEmail(String email) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public user saveUser(user user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<user> updateUser(user user) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+
 
 }
