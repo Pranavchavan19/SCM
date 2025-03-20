09c029f2fa7e27b2059bb276ba5df109582f9c01
Pranavbc <pranav1952000@gmail.com>
2025-03-20 10:30:15 +0530

    Springboot validation using annotation

--- scm2.0-main/src/main/java/com/scm/forms/UserForm.java
+++ scm2.0-main/src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +18,21 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @Email(message = "invalid email address")
     private String  email;
+
+    @NotBlank(message = "username Is reqired")
+    @Size( min=3 ,message = "Min 3 char is required")
     private String  name;
+
+    @NotBlank(message = "about is required")
     private String  about;
+
+    @Size(min=8 , max=12 , message = "Invalid phone number" )
     private String phoneNumber;
+
+    @NotBlank(message = "password is required")
+    @Size(min=6 , message = "Minimum 6 char is required")
     private String password;
 
 
